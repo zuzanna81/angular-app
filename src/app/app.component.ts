10eee169d5b82e88cf6a78cdaba9ecf1d6866511
Zuzanna <zuzanna.bienkowska@gmail.com>
2017-12-11 22:01:42 +0100

    Add steps according to course material week 7

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my first angular-app';
+  video = {
+    title: 'Despacito',
+    views: 2,
+    liked: true
+  }
+
+export class SuggesstionsComponent {
+  videos = [
+    {name: 'video one', liked: true}
+    {name: 'video two', liked: false}
+  ]
+
+  isShown: boolean = true
+
+  constructor() { }
+
+  handleClick(){
+    console.log('Someone clicked on button on the template :-)'
+  )
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { CommentComponent } from './comment/comment.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CommentComponent
   ],
   imports: [
     BrowserModule
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
